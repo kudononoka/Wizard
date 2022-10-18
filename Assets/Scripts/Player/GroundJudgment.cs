2db45d00834a454e66e470ee87e91389d8e694a4
Otsu01 <kudo.nonoka02@gmail.com>
2022-10-18 12:02:16 +0900

    接地判定、ジャンプ：完了 コントローラー操作に変更

---
+++ Assets/Scripts/Player/GroundJudgment.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>接地判定を行うためのクラス</summary>
+
+public class GroundJudgment : MonoBehaviour
+{
+    /// <summary>trueの時地面に接地しています</summary>
+    bool isGround; public bool IsGround => isGround;
+    /// <summary>接地判定を行うためのレイヤーです</summary>
+    [Header("地面とみなすオブジェクトのレイヤー"), SerializeField] LayerMask layerMask;
+    /// <summary>rayの長さ</summary>
+    float rayLength = 1.35f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, rayLength, layerMask))
+        {
+            isGround = true;
+        }
+        else
+        {
+            isGround = false;
+        }
+        
+        
+    }
+}
--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -9,34 +9,54 @@ public class PlayerMove : MonoBehaviour
     [Header("歩行速度"), SerializeField] float _walkSpeed;
     /// <summary>方向転換速度</summary>
     [Header("方向転換速度"), SerializeField] float _rotateSpeed;
+    /// <summary>ジャンプ力</summary>
+    [Header("ジャンプ力"), SerializeField] float _jumpPower;
 
     /// <summary>MainCamera</summary>
     GameObject _mainCamera;
 
     /// <summary>Playerの前方</summary>
     Quaternion _foward;
+
+    /// <summary>地面から離れた時かける重力</summary>
+    [Header("重力"), SerializeField] float _gravity;
+
+    GroundJudgment groundJudgment;
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
         _mainCamera = Camera.main.gameObject;
+        groundJudgment = GetComponent<GroundJudgment>();
     }
 
     
     void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float z = Input.GetAxisRaw("Vertical");
+        float x = Input.GetAxisRaw("HorizontalController");
+        float z = Input.GetAxisRaw("VerticalController");
 
-        //カメラのy軸のオイラー角を取得
-        Quaternion  _mainCamaraforward = Quaternion.AngleAxis(_mainCamera.transform.eulerAngles.y, Vector3.up); 
-        _rb.velocity = _mainCamaraforward.normalized * new Vector3(x * _walkSpeed, 0, z * _walkSpeed);
-
-        if(_rb.velocity != Vector3.zero)
+        if (groundJudgment.IsGround)
         {
-            _foward = Quaternion.LookRotation(_rb.velocity,Vector3.up);
+            //カメラのy軸のオイラー角を取得
+            Quaternion _mainCamaraforward = Quaternion.AngleAxis(_mainCamera.transform.eulerAngles.y, Vector3.up);
+            _rb.velocity = _mainCamaraforward.normalized * new Vector3(x * _walkSpeed, 0, -z * _walkSpeed);
+
+            if (_rb.velocity != Vector3.zero)
+            {
+                _foward = Quaternion.LookRotation(_rb.velocity, Vector3.up);
+            }
+
+            //滑らかに回転させる
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, _foward, _rotateSpeed * Time.deltaTime);
+
+            if(Input.GetButtonDown("JumpController"))
+            {
+                _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+            }
         }
-        
-        //滑らかに回転させる
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, _foward, _rotateSpeed * Time.deltaTime);
+        else
+        {
+            _rb.AddForce(Vector3.down * _gravity);
+        }
     }
 }
