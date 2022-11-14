5e353422a00e7deea853437fff000510fc8d794f
Otsu01 <kudo.nonoka02@gmail.com>
2022-11-14 13:33:48 +0900

    GameManagerのシングルトン化

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance = default;
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if(Instance)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
     void Start()
     {
         
