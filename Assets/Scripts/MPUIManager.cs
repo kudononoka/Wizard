8bf888add3c697319f9e002c111b4418f204cf42
Otsu01 <kudo.nonoka02@gmail.com>
2022-12-26 04:29:45 +0900

    MPUIManagerスクリプトのコードを見やすいように修正

--- Assets/Scripts/MPUIManager.cs
+++ Assets/Scripts/MPUIManager.cs
@@ -6,17 +6,16 @@ using System.Linq;
 
 public class MPUIManager : MonoBehaviour
 {
-    [SerializeField] GameObject _reafPrefab;
-    [SerializeField] Sprite _sprite;
-    [SerializeField] Sprite _sprite2;
-    [SerializeField] Sprite _sprite1;
-    [SerializeField] List<Image> _list = new List<Image>();
-    [SerializeField] List<Image> _list2 = new List<Image>();
+    [SerializeField] GameObject _leafPrefab;
+    [SerializeField] Sprite _leafHalfSprite;
+    [SerializeField] Sprite _leafNoneSprite;
+    [SerializeField] Sprite _leafNormalSprite;
+    [SerializeField] List<Image> _usebleMPLeaf = new List<Image>();
+    [SerializeField] List<Image> _MPLeaf = new List<Image>();
     PlayerHPMP _playerHPMP;
     GridLayoutGroup _group;
     RectTransform _rectTransform;
     int _reafNum;
-    // Start is called before the first frame update
     void Start()
     {
         _playerHPMP = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHPMP>();
@@ -24,6 +23,7 @@ public class MPUIManager : MonoBehaviour
         _rectTransform = GetComponent<RectTransform>();
         _reafNum = _playerHPMP.ReafNum;
         _group.constraintCount = _reafNum;
+        //葉っぱUIの数は１～１０までサイズや位置の調整可能・１１以上だとUIが崩れる可能性がある
         if(_reafNum > 7)
         {
             int n = _reafNum - 7;
@@ -37,59 +37,34 @@ public class MPUIManager : MonoBehaviour
         }
         for(var i = 0; i < _reafNum; i++)
         {
-            GameObject go =  Instantiate(_reafPrefab, this.transform);
+            GameObject go =  Instantiate(_leafPrefab, this.transform);
             Image sprite = go.GetComponent<Image>();
-            _list.Add(sprite);
-            _list2.Add(sprite);
+            //生成した葉っぱUIのSpriteをListに入れていく
+            _usebleMPLeaf.Add(sprite);　 //今使用できるMP(葉っぱ)のUIList
+            _MPLeaf.Add(sprite);　//生成した全ての葉っぱUIの保管場所
         }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void Changed(int amount)
+    public void Consumption(int amount)
     {
         int n = amount / 2;
         int n2 = amount % 2;
-        
-        if (transform.GetChild(_list.Count - 1).GetComponent<Image>().sprite == _sprite)
+        //使用できるMPの葉っぱUIの最後尾が半分の葉っぱUIの状態だったら
+        if (_usebleMPLeaf[_usebleMPLeaf.Count - 1].sprite == _leafHalfSprite)
         {
-            _list[_list.Count - 1].sprite = _sprite2;
-            //transform.GetChild(_list.Count - 1).GetComponent<Image>().sprite = _sprite2;
-            _list.RemoveAt(_list.Count - 1);
+             //半分の葉っぱUIを輪郭だけの葉っぱUIに変える
+            _usebleMPLeaf[_usebleMPLeaf.Count - 1].sprite = _leafNoneSprite;
+            _usebleMPLeaf.RemoveAt(_usebleMPLeaf.Count - 1);
             amount--;
             n = amount / 2;
             n2 = amount % 2;
 
-            for (var i = 1; i <= n; i++)
-            {
-                _list[_list.Count - 1].sprite = _sprite2;
-                //transform.GetChild(_list.Count - 1).GetComponent<Image>().sprite = _sprite2;
-                _list.RemoveAt(_list.Count - 1);
-            }
-            if (n2 == 1)
-            {
-                _list[_list.Count - 1].sprite = _sprite;
-                transform.GetChild(_list.Count - 1).GetComponent<Image>().sprite = _sprite;
-            }
+            MPConsumptionSpriteChange(n, n2);
         }
         else
         {
-            for (var i = 1; i <= n ; i++)
-            {
-                _list[_list.Count - 1].sprite = _sprite2;
-                //transform.GetChild(_list.Count - 1).GetComponent<Image>().sprite = _sprite2;
-                _list.RemoveAt(_list.Count - 1);
-            }
-            if (n2 == 1)
-            {
-                _list[_list.Count - 1].sprite = _sprite;
-                //transform.GetChild(_list.Count - 1).GetComponent<Image>().sprite = _sprite;
-            }
+            MPConsumptionSpriteChange(n, n2);
         }
         
     }
@@ -97,50 +72,57 @@ public class MPUIManager : MonoBehaviour
     {
         int n = amount / 2;
         int n2 = amount % 2;
-       
-        if (_list.Count != 0 && transform.GetChild(_list.Count - 1).GetComponent<Image>().sprite == _sprite)
+        //使用できるMPの葉っぱUIの最後尾が半分の葉っぱUIの状態だったら
+        if (_usebleMPLeaf.Count != 0 && _usebleMPLeaf[_usebleMPLeaf.Count - 1].sprite == _leafHalfSprite)
         {
-            _list[_list.Count - 1].sprite = _sprite1;
-            //transform.GetChild(_list.Count - 1).GetComponent<Image>().sprite = _sprite1;
+            _usebleMPLeaf[_usebleMPLeaf.Count - 1].sprite = _leafNormalSprite;
             amount--;
             n = amount / 2;
             n2 = amount % 2;
-            if (_list.Count < 10)
+            if (_usebleMPLeaf.Count < 10)
             {
                 for (var i = 1; i <= n; i++)
                 {
-                    var listSprite = _list2.Where(x => x.sprite == _sprite2).First();
-                    _list.Add(listSprite);
-                    _list[_list.Count - 1].sprite = _sprite1;
-                    //transform.GetChild(_list.Count - 1).GetComponent<Image>().sprite = _sprite1;
+                    MPRecoverySpriteChange(_leafNormalSprite);
                 }
                 if (n2 == 1)
                 {
-                    var listSprite = _list2.Where(x => x.sprite == _sprite2).First();
-                    _list.Add(listSprite);
-                    _list[_list.Count - 1].sprite = _sprite;
-                    //transform.GetChild(_list.Count - 1).GetComponent<Image>().sprite = _sprite;
+                    MPRecoverySpriteChange(_leafHalfSprite);
                 }
             }
         }
-        
         else
         {
-            for (var i = 1; i <= n && _list.Count < 10; i++)
+            for (var i = 1; i <= n && _usebleMPLeaf.Count < 10; i++)
             {
-                var listSprite = _list2.Where(x => x.sprite == _sprite2).First();
-                _list.Add(listSprite);
-                _list[_list.Count - 1].sprite = _sprite1;
-                //transform.GetChild(_list.Count - 1).GetComponent<Image>().sprite = _sprite1;
+                MPRecoverySpriteChange(_leafNormalSprite);
             }
-            if (n2 == 1 && _list.Count < 10)
+            if (n2 == 1 && _usebleMPLeaf.Count < 10)
             {
-                var listSprite = _list2.Where(x => x.sprite == _sprite2).First();
-                _list.Add(listSprite);
-                _list[_list.Count - 1].sprite = _sprite;
-                //transform.GetChild(_list.Count - 1).GetComponent<Image>().sprite = _sprite;
+                MPRecoverySpriteChange(_leafHalfSprite);
             }
 
         }
+    }
+    public void MPConsumptionSpriteChange(int n, int n2)
+    {
+        for (var i = 1; i <= n; i++)
+        {
+            _usebleMPLeaf[_usebleMPLeaf.Count - 1].sprite = _leafNoneSprite;
+            _usebleMPLeaf.RemoveAt(_usebleMPLeaf.Count - 1);
+        }
+        if (n2 == 1)
+        {
+            _usebleMPLeaf[_usebleMPLeaf.Count - 1].sprite = _leafHalfSprite;
+        }
+    }
+
+    public void MPRecoverySpriteChange(Sprite sprite)
+    {
+        //Listに入っている葉っぱUIの中で輪郭だけの葉っぱUIの先頭を変数に入れる
+        var listSprite = _MPLeaf.Where(x => x.sprite == _leafNoneSprite).First();
+        //新たに使用可能のMP葉っぱUIListにいれSpriteを半分の状態の葉っぱUIか普通の葉っぱUIに変える
+        _usebleMPLeaf.Add(listSprite);
+        _usebleMPLeaf[_usebleMPLeaf.Count - 1].sprite = sprite;
     }
 }
